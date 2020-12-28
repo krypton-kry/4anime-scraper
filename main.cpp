ece7a1e0abdfe9dbff6544907e996a1c1a70bdfe
krypton-kry <66370436+krypton-kry@users.noreply.github.com>
2020-12-28 03:57:30 -0500

    Updated  0.0.1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
+#include <bits/c++config.h>
 #include <cpr/cpr.h>
+#include <cpr/response.h>
 #include <htmlcxx/html/Node.h>
 #include <iostream>
 #include <htmlcxx/html/ParserDom.h>
+#include <utility>
+#include <vector>
+
+
+std::vector<std::string> search(cpr::Response response);
 
 int main(int argc, char** argv){
 
@@ -20,25 +27,46 @@ int main(int argc, char** argv){
       "qtranslate_lang=0&set_intitle=None&customset%5B%5D=anime"                  }
   });  
   
-
+//check status and do whatever you do
  if(resp.status_code == 200){
-   htmlcxx::HTML::ParserDom parser;
-   tree<htmlcxx::HTML::Node> dom = parser.parseTree(resp.text);
+   std::vector<std::string> anime_links;
 
-   tree<htmlcxx::HTML::Node>::iterator iter = dom.begin();
-   tree<htmlcxx::HTML::Node>::iterator end = dom.end(); 
-   for(; iter!=end; ++iter){
-    if(iter->tagName() == "img"){
-      iter->parseAttributes(); 
-      std::cout << "https://4anime.to" <<iter->attribute("src").second << std::endl;
-    }
-   }
-
- }else{
+   anime_links = search(resp);
+    
+ } else {
    std::cout 
      << "\n[ERROR] Status Code : " 
      << resp.status_code << "Whlie searching for results!" << std::endl;
  }
   
   return 0;
+}
+
+std::vector<std::string> search(cpr::Response response){
+  
+   std::vector<std::string> anime_loc;
+
+   htmlcxx::HTML::ParserDom parser;
+   tree<htmlcxx::HTML::Node> dom = parser.parseTree(response.text);
+
+   tree<htmlcxx::HTML::Node>::iterator iter = dom.begin();
+   tree<htmlcxx::HTML::Node>::iterator end = dom.end();
+    
+   for(; iter!=end; ++iter){
+    
+    if(iter->tagName() == "a"){
+     
+      iter->parseAttributes();
+      
+      std::string a_href = iter->attribute("href").second;
+   
+      if(a_href.find("https://4anime.to/") != std::string::npos){
+          
+          anime_loc.push_back(a_href);       
+          
+          std::cout << a_href << std::endl;
+      }
+    }
+  }
+   return anime_loc; 
 }
